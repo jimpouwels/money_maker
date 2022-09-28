0a7cf2487684add0169bf6ba83f35a29248d4181
Jim Pouwels <jim.pouwels@philips.com>
2022-09-28 10:23:56 +0200

    removed console log

--- src/controller/statistics_controller.js
+++ src/controller/statistics_controller.js
@@ -3,7 +3,6 @@ export default class StatisticsController {
     constructor(app, statisticsService) {
         app.get('/statistics', (req, res) => {
             console.log(`[/statistic] Post statistic`);
-            console.log(req.body);
             let statistics = statisticsService.getStatistics();
             let displayString = "";
             displayString += `Total clicks: ${statistics.totalClicks}<br /><br />`;
