bd1c6c318d94cc576babc6d644e61c60a9e83b22
Jim Pouwels <jim.pouwels@philips.com>
2022-10-12 08:17:02 +0200

    clix today

--- frontend/src/components/totals.jsx
+++ frontend/src/components/totals.jsx
@@ -33,20 +33,20 @@ export default function Totals({ data }) {
                 <table>
                     <tbody>
                         <tr>
-                            <th scope="row">Total number of clicks:</th>
+                            <th scope="row">Total clix:</th>
                             <td>{String(data.totalClicks)}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Total profit (min):</th>
-                            <td>&euro; {toString(data.totalClicks * 0.005)}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Total profit (max):</th>
-                            <td>&euro; {toString(data.totalClicks * 0.01)}</td>
                         </tr>
                         <tr>
                             <th scope="row">Clix today:</th>
                             <td>{clixToday}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Minimum profit:</th>
+                            <td>&euro; {toString(data.totalClicks * 0.005)}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Maximum profit:</th>
+                            <td>&euro; {toString(data.totalClicks * 0.01)}</td>
                         </tr>
                     </tbody>
                 </table>
