69a83f369e9d5647c3e9d3a9b13b0c9ceaffce4f
Jim Pouwels <jim.pouwels@philips.com>
2022-10-31 15:46:37 +0100

    implemented url util

--- backend/src/util/url_util.js
+++ backend/src/util/url_util.js
@@ -1,7 +1,24 @@
 export default class UrlUtil {
 
     static parse(url) {
-        
+        let parts = url.split('://');
+        let domainAndPath = parts[1].split('/');
+        const domain = domainAndPath[0];
+        const pathAndQueryString = domainAndPath.length > 1 ? domainAndPath[1].split('?') : [];
+        let path = pathAndQueryString.length > 0 ? pathAndQueryString[0] : '';
+        let queryString = pathAndQueryString.length > 1 ? pathAndQueryString[1] : '';
+        const queryParams = [];
+        for (let queryParam of queryString.split('&')) {
+            let queryParamParts = queryParam.split('=');
+            queryParams.push({ name: queryParamParts[0], value: queryParamParts[1] });
+        }
+        return {
+            protocol: parts[0],
+            host: domain,
+            path: path,
+            queryString: queryString,
+            queryParams: queryParams
+        }
     }
 
 }
--- backend/test/util/url_util.spec.js
+++ backend/test/util/url_util.spec.js
@@ -3,6 +3,14 @@ import UrlUtil from "../../src/util/url_util";
 describe("url util", () => {
 
     it("should parse the passed URL", () => {
-        let parsedURL = UrlUtil.parse('http://www.google.com');
+        let parsedURL = UrlUtil.parse('http://www.google.com/thePath?param1=value1&param2=value2');
+        expect(parsedURL.host).toEqual('www.google.com');
+        expect(parsedURL.protocol).toEqual('http');
+        expect(parsedURL.path).toEqual('thePath');
+        expect(parsedURL.queryString).toEqual('param1=value1&param2=value2');
+        expect(parsedURL.queryParams[0].name).toEqual('param1');
+        expect(parsedURL.queryParams[0].value).toEqual('value1');
+        expect(parsedURL.queryParams[1].name).toEqual('param2');
+        expect(parsedURL.queryParams[1].value).toEqual('value2');
     });
 });
