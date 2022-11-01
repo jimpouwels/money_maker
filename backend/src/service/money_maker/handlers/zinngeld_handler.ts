1c2c3b1ff0b744be56dc663688f89b39e17d1f22
Jim Pouwels <jim.pouwels@philips.com>
2022-11-01 16:31:33 +0100

    zinngeld to ts

---
+++ backend/src/service/money_maker/handlers/zinngeld_handler.ts
@@ -0,0 +1,31 @@
+import { Browser, Page } from "../../../../node_modules/puppeteer/lib/types.js";
+import Mail from "../../../domain/mail.js";
+import Url from "../../../domain/url.js";
+import Handler from "./handler.js";
+
+export default class ZinnGeldHandler extends Handler {
+    
+    constructor(name: string) {
+        super(name);
+    }
+
+    public matchUrl(url: Url): boolean {
+        return url.host.includes('zinngeld') && url.path.includes('maillink');
+    }
+
+    public async performCustomAction(_page: Page, _url: Url, _browser: Browser): Promise<void> {
+    }
+    
+    public hasRedirected(url: Url): boolean {
+        return super.hasRedirected(url) && true;
+    }
+
+    public filter(_mail: Mail): boolean {
+        return false;
+    }
+
+    protected getSkipSubjects(): string[] {
+        return [];
+    }
+
+}
