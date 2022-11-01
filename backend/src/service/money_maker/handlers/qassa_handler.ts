058d6d7bd730b4869277e27549fceba76ebd2674
Jim Pouwels <jim.pouwels@philips.com>
2022-11-01 16:28:21 +0100

    qassa handler to TS

---
+++ backend/src/service/money_maker/handlers/qassa_handler.ts
@@ -0,0 +1,31 @@
+import { Browser, Page } from "../../../../node_modules/puppeteer/lib/types.js";
+import Mail from "../../../domain/mail.js";
+import Url from "../../../domain/url.js";
+import Handler from "./handler.js";
+
+export default class QassaHandler extends Handler {
+
+    constructor(name: string) {
+        super(name);
+    }
+
+    public matchUrl(url: Url): boolean {
+        return url.path.includes('/klik/');
+    }
+
+    public async performCustomAction(_page: Page, _url: Url, _browser: Browser): Promise<void> {
+    }
+    
+    public hasRedirected(url: Url): boolean {
+        return super.hasRedirected(url) && !url.host.includes('qassa');
+    }
+
+    public filter(_mail: Mail): boolean {
+        return false;
+    }
+
+    protected getSkipSubjects(): string[] {
+        return [];
+    }
+
+}
