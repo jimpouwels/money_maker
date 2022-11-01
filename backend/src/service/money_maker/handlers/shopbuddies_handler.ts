70591ff2d794d8e90662b5292b6ea3cf144a7439
Jim Pouwels <jim.pouwels@philips.com>
2022-11-01 16:30:24 +0100

    shopbuddies to TS

---
+++ backend/src/service/money_maker/handlers/shopbuddies_handler.ts
@@ -0,0 +1,31 @@
+import { Browser, Page } from "../../../../node_modules/puppeteer/lib/types.js";
+import Mail from "../../../domain/mail.js";
+import Url from "../../../domain/url.js";
+import Handler from "./handler.js";
+
+export default class ShopBuddiesHandler extends Handler {
+    
+    constructor(name: string) {
+        super(name);
+    }
+
+    public matchUrl(url: Url): boolean {
+        return url.host.includes('shopbuddies') && url.path.includes('newsletter_exit');
+    }
+
+    public async performCustomAction(_page: Page, _url: Url, _browser: Browser): Promise<void> {
+    }
+    
+    public hasRedirected(url: Url): boolean {
+        return super.hasRedirected(url) && !url.host.includes('shopbuddies.nl');
+    }
+
+    public filter(mail: Mail): boolean {
+        return this.getSkipSubjects().find(subject => mail.subject.toLowerCase().includes(subject)) != null;
+    }
+
+    protected getSkipSubjects(): string[] {
+        return ['ledenactie'];
+    }
+
+}
