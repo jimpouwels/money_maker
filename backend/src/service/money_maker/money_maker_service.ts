26dcfaa7f7a094de71c639894daa3a1680a57285
Jim Pouwels <jim.pouwels@gmail.com>
2025-04-01 09:24:34 +0200

    Update money_maker_service.ts

--- backend/src/service/money_maker/money_maker_service.ts
+++ backend/src/service/money_maker/money_maker_service.ts
@@ -46,7 +46,6 @@ export default class MoneyMakerService {
         this.handlers.push(new GeldraceHandler('GeldRace'));
         this.handlers.push(new OnlineLeadsHandler('BespaarPortaal', 'bespaarportaal.nl'));
         this.handlers.push(new OnlineLeadsHandler('DirectVerdiend', 'directverdiend.nl'));
-        this.handlers.push(new ShopBuddiesHandler('ShopBuddies'));
         this.handlers.push(new QassaHandler('Qassa'));
         this.urlExtractor = new UrlExtractor();
     }
